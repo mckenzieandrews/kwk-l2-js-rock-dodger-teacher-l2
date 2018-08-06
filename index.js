e1b87c890b37e44e7bfddfba013bffcf8de28828
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-06 12:25:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -120,7 +120,18 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    
-   
+   const code = e.which
+
+  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  if (code === LEFT_ARROW) {
+    moveDodgerLeft()
+  } else if (code === RIGHT_ARROW) {
+    moveDodgerRight()
+  }
 }
 
 function moveDodgerLeft() {
